9329488a23c7f76cc7529383d89b850694a0afaa
Andreas Pfeil <patreon@familie-pfeil.com>
2021-07-28 18:14:45 +0200

    ADD: SDK Prototype using native C SDK

--- GO/src/Test/test1.go
+++ GO/src/Test/test1.go
@@ -2,22 +2,48 @@ package main
 
 import "os"
 import "fmt"
+//import "strings"
 import "sqlitecloud"
+import "encoding/json"
 
 
 func main() {
 	fmt.Printf( "Simple API test...\r\n")
 
-	db, err := sqlitecloud.Connect( "dev1.sqlitecloud.io", 8860, "", "", "Test", 10, 0 )
+	db, err := sqlitecloud.Connect( "sqlitecloud://dev1.sqlitecloud.io/X" )
+
+	//db := sqlitecloud.New()
+	//err := db.Connect( "dev1.sqlitecloud.io", 8860, "", "", "X", 10, 0 )
+
+
 	if err == nil {
 		defer db.Close()
 
-		fmt.Printf( "UUID = '%s'\r\n", db.GetCloudUUID() )
 
-		db.Use( "Test" )
-		res, err := db.Execute( "SELECT * FROM Dummy" )
+		if db.Ping() == nil {
+			fmt.Println( "PONG." )
+		}
+
+		db.UseDatabase( "X" )
+
+		commands, _ := db.ListCommands()
+		jCommands, _ := json.Marshal( commands )
+		fmt.Printf( "LIST COMMANDS:\r\n%v\r\n", string( jCommands ) )
+	
+		info, _ := db.ListInfo()
+		jInfo, _ := json.Marshal( info )
+		fmt.Printf( "LIST INFO:\r\n%v\r\n", string( jInfo ) )
+
+		tables, _ := db.ListTables()
+		jTables, _ := json.Marshal( tables )
+		fmt.Printf( "LIST TABLES:\r\n%v\r\n", string( jTables ) )
+
+		fmt.Printf( "UUID = '%s'\r\n", db.GetUUID() )
+
+		
+		res, err := db.Select( "SELECT * FROM Dummy" )
 		if( err == nil ) {
-			fmt.Printf( "Num Rows = %d\r\n", res.GetNumRows() )
+			fmt.Printf( "Num Rows = %d\r\n", res.GetNumberOfRows() )
 			res.Dump( 80 )
 			res.Free()
 			os.Stderr.WriteString("\r\n")
--- GO/src/sqlitecloud/c.go
+++ GO/src/sqlitecloud/c.go
@@ -11,11 +11,31 @@ import "unsafe"
 // import "reflect"
 
 type SQCloud struct {
-  connection 	*C.struct_SQCloudConnection
+  connection 		*C.struct_SQCloudConnection
+
+	Host      		string
+	Port		  		int
+	Username			string
+	Password			string
+	Database  		string
+  Timeout   		int
+	Family    		int
+
+	UUID          string
+
+	ErrorCode   	int
+	ErrorMessage 	string
 }
 
 type SQCloudResult struct {
 	result *C.struct_SQCloudResult
+
+	row uint
+	Rows uint
+
+	Type 	int
+	ErrorCode int
+	ErrorMessage string
 }
 
 // SQCloudResType
@@ -36,7 +56,7 @@ const VALUE_BLOB 			= C.VALUE_BLOB
 const VALUE_NULL 			= C.VALUE_NULL
 
 // SQCloudConnection *SQCloudConnect (const char *hostname, int port, SQCloudConfig *config);
-func bridge_Connect( Host string, Port int, Username string, Password string, Database string, Timeout int, Family int ) *SQCloud {
+func CConnect( Host string, Port int, Username string, Password string, Database string, Timeout int, Family int ) *C.struct_SQCloudConnection {
 	conf := C.struct_SQCloudConfigStruct{}
 	conf.username = C.CString( Username )
 	conf.password = C.CString( Password )
@@ -46,12 +66,21 @@ func bridge_Connect( Host string, Port int, Username string, Password string, Da
 
 	cHost := C.CString( Host )
 
-	connection := SQCloud{ connection: C.SQCloudConnect( cHost, C.int( Port ), nil ) }
+	cConnection := C.SQCloudConnect( cHost, C.int( Port ), &conf )
 	
 	C.free( unsafe.Pointer( cHost ) )
 	C.free( unsafe.Pointer( conf.database ) )
 	C.free( unsafe.Pointer( conf.password ) )
 	C.free( unsafe.Pointer( conf.username ) )
+
+  return cConnection
+}
+
+// SQCloudConnection *SQCloudConnectWithString (const char *s);
+func CConnectWithString( ConnectionString string ) *SQCloud {
+	cConString := C.CString( ConnectionString )
+	connection := SQCloud{ connection: C.SQCloudConnectWithString( cConString ) }
+	C.free( unsafe.Pointer( cConString ) )
 
 	if connection.connection == nil {
 		return nil
@@ -59,37 +88,38 @@ func bridge_Connect( Host string, Port int, Username string, Password string, Da
 
   return &connection
 }
+
 // void SQCloudDisconnect (SQCloudConnection *connection);
-func (this *SQCloud ) bridge_Disconnect() {
+func (this *SQCloud ) CDisconnect() {
 	if this.connection != nil {
 		C.SQCloudDisconnect( this.connection )
 		this.connection = nil
 	}
 }
 // char *SQCloudUUID (SQCloudConnection *connection);
-func (this *SQCloud ) bridge_GetCloudUUID() string {
+func (this *SQCloud ) CGetCloudUUID() string {
 	 return C.GoString( C.SQCloudUUID( this.connection ) )
 }
 
 //bool SQCloudIsError (SQCloudConnection *connection);
-func (this *SQCloud ) bridge_IsError() bool {
+func (this *SQCloud ) CIsError() bool {
 	return bool( C.SQCloudIsError( this.connection ) )
 }
 //int SQCloudErrorCode (SQCloudConnection *connection);
-func (this *SQCloud ) bridge_GetErrorCode() int {
+func (this *SQCloud ) CGetErrorCode() int {
 	return int( C.SQCloudErrorCode( this.connection ) )
 }
 //const char *SQCloudErrorMsg (SQCloudConnection *connection);
-func (this *SQCloud ) bridge_GetErrorMessage() string {
+func (this *SQCloud ) CGetErrorMessage() string {
 	return C.GoString( C.SQCloudErrorMsg( this.connection ) )
 }
 
 // SQCloudResult *SQCloudExec (SQCloudConnection *connection, const char *command);
-func (this *SQCloud ) bridge_Exec( Command string ) *SQCloudResult {
+func (this *SQCloud ) CExec( Command string ) *SQCloudResult {
 	cCommand := C.CString( Command )
   defer C.free( unsafe.Pointer( cCommand ) )
 
-	println( "exec ("+Command+").." )
+	// println( "exec ("+Command+").." )
 
 	result := SQCloudResult{ result: C.SQCloudExec( this.connection, cCommand ) }
 	if result.result == nil {
@@ -98,7 +128,7 @@ func (this *SQCloud ) bridge_Exec( Command string ) *SQCloudResult {
 	return &result
 }
 // SQCloudResult *SQCloudSetPubSubOnly (SQCloudConnection *connection);
-func (this *SQCloud ) bridge_SetPubSubOnly() *SQCloudResult {
+func (this *SQCloud ) CSetPubSubOnly() *SQCloudResult {
 	result := SQCloudResult{ result: C.SQCloudSetPubSubOnly( this.connection ) }
 	
 	if result.result == nil {
@@ -108,69 +138,69 @@ func (this *SQCloud ) bridge_SetPubSubOnly() *SQCloudResult {
 	return &result
 }
 // SQCloudResType SQCloudResultType (SQCloudResult *result);
-func (this *SQCloudResult ) bridge_GetResultType() int {
+func (this *SQCloudResult ) CGetResultType() int {
 	return int( C.SQCloudResultType( this.result ) )
 }
 // uint32_t SQCloudResultLen (SQCloudResult *result);
-func (this *SQCloudResult ) bridge_GetResultLen() uint {
+func (this *SQCloudResult ) CGetResultLen() uint {
 	return uint( C.SQCloudResultLen( this.result ) )
 }
 // char *SQCloudResultBuffer (SQCloudResult *result);
-func (this *SQCloudResult ) bridge_GetResultBuffer() string {
+func (this *SQCloudResult ) CGetResultBuffer() string {
 	return C.GoString( C.SQCloudResultBuffer( this.result ) )
 }
 // void SQCloudResultFree (SQCloudResult *result);
-func (this *SQCloudResult ) bridge_Free() {
+func (this *SQCloudResult ) CFree() {
 	C.SQCloudResultFree( this.result )
 }
 // bool SQCloudResultIsOK (SQCloudResult *result);
-func (this *SQCloudResult ) bridge_IsOK() bool {
+func (this *SQCloudResult ) CIsOK() bool {
 	return bool( C.SQCloudResultIsOK( this.result ) )
 }
 // SQCloudValueType SQCloudRowsetValueType (SQCloudResult *result, uint32_t row, uint32_t col);
-func (this *SQCloudResult ) bridge_GetValueType( Row uint, Column uint ) int {
+func (this *SQCloudResult ) CGetValueType( Row uint, Column uint ) int {
 	return int( C.SQCloudRowsetValueType( this.result, C.uint( Row ), C.uint( Column ) ) )
 }
 // char *SQCloudRowsetColumnName (SQCloudResult *result, uint32_t col, uint32_t *len);
-func (this *SQCloudResult ) bridge_GetColumnName( Row uint, Column uint ) string {
+func (this *SQCloudResult ) CGetColumnName( Row uint, Column uint ) string {
 	var len C.uint32_t = 0
 	return C.GoStringN( C.SQCloudRowsetColumnName( this.result, C.uint( Column ), &len ), C.int( len ) )
 }
 // uint32_t SQCloudRowsetRows (SQCloudResult *result);
-func (this *SQCloudResult ) bridge_GetRows() uint {
+func (this *SQCloudResult ) CGetRows() uint {
 	return uint( C.SQCloudRowsetRows( this.result ) )
 }
 // uint32_t SQCloudRowsetCols (SQCloudResult *result);
-func (this *SQCloudResult ) bridge_GetColumns() uint {
+func (this *SQCloudResult ) CGetColumns() uint {
 	return uint( C.SQCloudRowsetCols( this.result ) )
 }
 // uint32_t SQCloudRowsetMaxLen (SQCloudResult *result);
-func (this *SQCloudResult ) bridge_GetMaxLen() uint32 {
+func (this *SQCloudResult ) CGetMaxLen() uint32 {
 	return uint32( C.SQCloudRowsetMaxLen( this.result ) )
 }
 // char *SQCloudRowsetValue (SQCloudResult *result, uint32_t row, uint32_t col, uint32_t *len);
-func (this *SQCloudResult ) bridge_GetStringValue( Row uint, Column uint ) string {
+func (this *SQCloudResult ) CGetStringValue( Row uint, Column uint ) string {
 	var len C.uint32_t = 0
   return C.GoStringN( C.SQCloudRowsetValue( this.result, C.uint32_t( Row ), C.uint32_t( Column ), &len ), C.int( len ) ) // Problem: NULL Pointer in return
 }
 // int32_t SQCloudRowsetInt32Value (SQCloudResult *result, uint32_t row, uint32_t col);
-func (this *SQCloudResult ) bridge_GetInt32Value( Row uint, Column uint ) int32 {
+func (this *SQCloudResult ) CGetInt32Value( Row uint, Column uint ) int32 {
 	return int32( C.SQCloudRowsetInt32Value( this.result, C.uint( Row ), C.uint( Column ) ) )
 }
 // int64_t SQCloudRowsetInt64Value (SQCloudResult *result, uint32_t row, uint32_t col);
-func (this *SQCloudResult ) bridge_GetInt64Value( Row uint, Column uint ) int64 {
+func (this *SQCloudResult ) CGetInt64Value( Row uint, Column uint ) int64 {
 	return int64( C.SQCloudRowsetInt64Value( this.result, C.uint( Row ), C.uint( Column ) ) )
 }
 // float SQCloudRowsetFloatValue (SQCloudResult *result, uint32_t row, uint32_t col);
-func (this *SQCloudResult ) bridge_GetFloat32Value( Row uint, Column uint ) float32 {
+func (this *SQCloudResult ) CGetFloat32Value( Row uint, Column uint ) float32 {
 	return float32( C.SQCloudRowsetFloatValue( this.result, C.uint( Row ), C.uint( Column ) ) )
 }
 // double SQCloudRowsetDoubleValue (SQCloudResult *result, uint32_t row, uint32_t col);
-func (this *SQCloudResult ) bridge_GetFloat64Value( Row uint, Column uint ) float64 {
+func (this *SQCloudResult ) CGetFloat64Value( Row uint, Column uint ) float64 {
 	return float64( C.SQCloudRowsetDoubleValue( this.result, C.uint( Row ), C.uint( Column ) ) )
 }
 // void SQCloudRowsetDump (SQCloudResult *result, uint32_t maxline);
-func (this *SQCloudResult ) bridge_Dump( MaxLine uint ) {
+func (this *SQCloudResult ) CDump( MaxLine uint ) {
 	 C.SQCloudRowsetDump( this.result, C.uint( MaxLine ) )
 }
 
