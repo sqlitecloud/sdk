23d197b6da33f5ebbf6afc990e360de37729835c
Marco Bambini <marco@creolabs.com>
2021-08-03 15:03:51 +0200

    Updated SDK reserved4 function for internal Raft usage

--- C/sqcloud_private.h
+++ C/sqcloud_private.h
@@ -19,7 +19,7 @@
 bool SQCloudForwardExec(SQCloudConnection *connection, const char *command, bool (*forward_cb) (char *buffer, size_t blen, void *xdata), void *xdata);
 SQCloudResult *SQCloudSetUUID (SQCloudConnection *connection, const char *UUID);
 
-SQCloudResult *sqcloud_parse_buffer (char *buffer, uint32_t blen, uint32_t cstart);
+SQCloudResult *sqcloud_parse_buffer (char *buffer, uint32_t blen, uint32_t cstart, SQCloudResult *chunk);
 uint32_t sqcloud_parse_number (char *buffer, uint32_t blen, uint32_t *cstart);
 
 #endif
