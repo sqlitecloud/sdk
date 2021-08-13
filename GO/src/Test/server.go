6e119a608a41c546810f1fcf8596df8ea31475d2
Andreas Pfeil <patreon@familie-pfeil.com>
2021-08-13 14:29:51 +0200

    ADD: XML output rendering, Test/server.go, Test/dump.go, FIX: row rendering

--- GO/src/Test/server.go
+++ GO/src/Test/server.go
@@ -1,3 +1,20 @@
+//
+//                    ////              SQLite Cloud
+//        ////////////  ///             
+//      ///             ///  ///        Product     : SQLite Cloud GO SDK
+//     ///             ///  ///         Version     : 0.0.1
+//     //             ///   ///  ///    Date        : 2021/08/13
+//    ///             ///   ///  ///    Author      : Andreas Pfeil
+//   ///             ///   ///  ///     
+//   ///     //////////   ///  ///      Description : Test program for the 
+//   ////                ///  ///                     SQLite Cloud server 
+//     ////     //////////   ///                      commands.
+//        ////            ////          
+//          ////     /////              
+//             ///                      Copyright   : 2021 by SQLite Cloud Inc.
+//
+// -----------------------------------------------------------------------TAB=2
+
 package main
 
 import "os"
@@ -8,228 +25,228 @@ import "sqlitecloud"
 
 
 func main() {
-	fmt.Printf( "Server API test...\r\n")
+  fmt.Printf( "Server API test...\r\n")
 
-	db := sqlitecloud.New()
+  db := sqlitecloud.New()
 
-	err := db.Connect( "***REMOVED***", 8860, "none empty user", "none empty password", "X", 10, "NO", 0 ) // Host, Port, Username, Password, Database, Timeout, Compression, Family
-	if err == nil {
-		defer db.Close()
+  err := db.Connect( "***REMOVED***", 8860, "none empty user", "none empty password", "X", 10, "NO", 0 ) // Host, Port, Username, Password, Database, Timeout, Compression, Family
+  if err == nil {
+    defer db.Close()
 
-		fmt.Printf( "Checking PING..." )
-		if db.Ping() != nil {
-			fail( err.Error() )
-		}
-		fmt.Printf( "ok.\r\n" )
+    fmt.Printf( "Checking PING..." )
+    if db.Ping() != nil {
+      fail( err.Error() )
+    }
+    fmt.Printf( "ok.\r\n" )
 
-		fmt.Printf( "Checking AUTH..." )
-		if err := db.Auth( "pfeil", "secret"); err != nil { //  Username, Password
-			fail( err.Error() )
-		}
-		fmt.Printf( "ok.\r\n" )
-
-
-
-
-
-		fmt.Printf( "Checking CREATE DATABASE..." )
-		if err := db.CreateDatabase( "xyz", "", "", false ); err != nil { // Database, Key, Encoding, NoError
-			fail( err.Error() )
-		} 
-		fmt.Printf( "ok.\r\n" )
-
-		fmt.Printf( "Checking LIST DATABASES..." )
-		if databases, err := db.ListDatabases(); err != nil {
-			fail( err.Error() )
-		} else {
-			if len( databases ) == 0 {
-				fail( "Ivalid result." )
-			}
-		}
-		fmt.Printf( "ok.\r\n" )
-
-		fmt.Printf( "Checking DROP DATABASE..." )
-		if err := db.DropDatabase( "xyz", false ); err != nil { // Database, NoError
-			fail( err.Error() )
-		}
-		fmt.Printf( "ok.\r\n" )
-		
-
-
-		fmt.Printf( "Checking USE DATABASE..." )
-		db.CreateDatabase( "X", "", "", true )
-		if err := db.UseDatabase( "X" ); err != nil { // Database
-			fail( err.Error() )
-		}
-		fmt.Printf( "ok.\r\n" )
-
-		fmt.Printf( "Checking GET DATABASE ID..." )
-		id, err := db.GetDatabaseID()
-		if err != nil {
-			fail( err.Error() )
-		}
-		fmt.Printf( "ok.\r\n" )
-
-
-		fmt.Printf( "Checking SET KEY..." )
-		if err := db.SetKey( "A", "1405" ); err != nil { // Key, Value
-			fail( err.Error() )
-		} 
-		fmt.Printf( "ok.\r\n" )
-
-		fmt.Printf( "Checking LIST KEYS..." )
-		if keys, err := db.ListKeys() ; err != nil { 
-			fail( err.Error() )
-		} else {
-			if len( keys ) != 1 {
-				fail( "Invalid amount of keys found." )
-			}
-		}
-		fmt.Printf( "ok.\r\n" )
-
-		fmt.Printf( "Checking GET KEY..." )
-		if val, err := db.GetKey( "A" ) ; err != nil { 
-			fail( err.Error() )
-		} else {
-			if val != "1405" {
-				fail( fmt.Sprintf( "Invalid value (%s).", val ) )
-			}
-		}
-		fmt.Printf( "ok.\r\n" )
-
-		fmt.Printf( "Checking DROP KEY..." )
-		if err := db.DropKey( "A" ); err != nil {
-			fail( err.Error() )
-		} else {
-			if keys, err := db.ListKeys() ; err != nil {
-				fail( err.Error() )
-			} else {
-				if len( keys ) != 0 {
-					fail( "Key could not be deleted." )
-				}
-			}
-			fmt.Printf( "ok.\r\n" )
-		}
-
-
-		
-
-
-		fmt.Printf( "Checking LIST COMMANDS..." )
-		if commands, err := db.ListCommands(); err != nil {
-			fail( err.Error() )
-		} else {
-			if len( commands ) != 27 {
-				fail( "Ivalid result." )
-			}
-		}
-		fmt.Printf( "ok.\r\n" )
-
-
-		fmt.Printf( "Checking LIST CONNECTIONS..." )
-		if connections, err := db.ListConnections(); err != nil {
-			fail( err.Error() )
-		} else {
-			if len( connections ) == 0 {
-				fail( "Ivalid result." )
-			}
-		}
-		fmt.Printf( "ok.\r\n" )
-
-		fmt.Printf( "Checking LIST DATABASE CONNECTIONS (X)..." )
-		if connections, err := db.ListDatabaseConnections( "X" ); err != nil {
-			fail( err.Error() )
-		} else {
-			if len( connections ) == 0 {
-				fail( "Ivalid result." )
-			}
-		}
-		fmt.Printf( "ok.\r\n" )
-
-
-	
-
-		fmt.Printf( "Checking LIST DATABASE CONNECTIONS ID %d...", id )
-		if connections, err := db.ListDatabaseClientConnectionIds( id ); err != nil {
-			fail( err.Error() )
-		} else {
-			if len( connections ) == 0 {
-				fail( "Ivalid result." )
-			}
-		}
-		fmt.Printf( "ok.\r\n" )
-
-		fmt.Printf( "Checking LIST INFO..." )
-		if _, err := db.GetInfo(); err != nil {
-			fail( err.Error() )
-		}
-		fmt.Printf( "ok.\r\n" )
-
-		fmt.Printf( "Checking LIST TABLES..." )
-		if tables, err := db.ListTables(); err != nil {
-			fail( err.Error() )
-		} else {
-			if len( tables ) < 1 {
-				fail( "Ivalid result." )
-			}
-		}
-		fmt.Printf( "ok.\r\n" )
-
-		fmt.Printf( "Checking LIST PLUGINS..." )
-		if _, err := db.ListPlugins(); err != nil {
-			fail( err.Error() )
-		}
-		fmt.Printf( "ok.\r\n" )
-	
-
-		fmt.Printf( "Checking LIST CLIENT KEYS..." )
-		if _, err := db.ListClientKeys(); err != nil {
-			fail( err.Error() )
-		}
-		fmt.Printf( "ok.\r\n" )
+    fmt.Printf( "Checking AUTH..." )
+    if err := db.Auth( "pfeil", "secret"); err != nil { //  Username, Password
+      fail( err.Error() )
+    }
+    fmt.Printf( "ok.\r\n" )
 
 
 
 
-		// fmt.Printf( "Checking LIST NODES..." )
-		// if nodes, err := db.ListNodes(); err != nil {
-		// 	fail( err.Error() )
-		// } else {
-		// 	if len( nodes ) == 0 {
-		// 		fail( "Ivalid result." )
-		// 	}
-		// }
-		// fmt.Printf( "ok.\r\n" )
 
-		fmt.Printf( "Checking LIST DATABASE KEYS..." )
-		if _, err := db.ListDatabaseKeys(); err != nil {
-			fail( err.Error() )
-		}
-		fmt.Printf( "ok.\r\n" )
+    fmt.Printf( "Checking CREATE DATABASE..." )
+    if err := db.CreateDatabase( "xyz", "", "", false ); err != nil { // Database, Key, Encoding, NoError
+      fail( err.Error() )
+    } 
+    fmt.Printf( "ok.\r\n" )
+
+    fmt.Printf( "Checking LIST DATABASES..." )
+    if databases, err := db.ListDatabases(); err != nil {
+      fail( err.Error() )
+    } else {
+      if len( databases ) == 0 {
+        fail( "Ivalid result." )
+      }
+    }
+    fmt.Printf( "ok.\r\n" )
+
+    fmt.Printf( "Checking DROP DATABASE..." )
+    if err := db.DropDatabase( "xyz", false ); err != nil { // Database, NoError
+      fail( err.Error() )
+    }
+    fmt.Printf( "ok.\r\n" )
+    
+
+
+    fmt.Printf( "Checking USE DATABASE..." )
+    db.CreateDatabase( "X", "", "", true )
+    if err := db.UseDatabase( "X" ); err != nil { // Database
+      fail( err.Error() )
+    }
+    fmt.Printf( "ok.\r\n" )
+
+    fmt.Printf( "Checking GET DATABASE ID..." )
+    id, err := db.GetDatabaseID()
+    if err != nil {
+      fail( err.Error() )
+    }
+    fmt.Printf( "ok.\r\n" )
+
+
+    fmt.Printf( "Checking SET KEY..." )
+    if err := db.SetKey( "A", "1405" ); err != nil { // Key, Value
+      fail( err.Error() )
+    } 
+    fmt.Printf( "ok.\r\n" )
+
+    fmt.Printf( "Checking LIST KEYS..." )
+    if keys, err := db.ListKeys() ; err != nil { 
+      fail( err.Error() )
+    } else {
+      if len( keys ) != 1 {
+        fail( "Invalid amount of keys found." )
+      }
+    }
+    fmt.Printf( "ok.\r\n" )
+
+    fmt.Printf( "Checking GET KEY..." )
+    if val, err := db.GetKey( "A" ) ; err != nil { 
+      fail( err.Error() )
+    } else {
+      if val != "1405" {
+        fail( fmt.Sprintf( "Invalid value (%s).", val ) )
+      }
+    }
+    fmt.Printf( "ok.\r\n" )
+
+    fmt.Printf( "Checking DROP KEY..." )
+    if err := db.DropKey( "A" ); err != nil {
+      fail( err.Error() )
+    } else {
+      if keys, err := db.ListKeys() ; err != nil {
+        fail( err.Error() )
+      } else {
+        if len( keys ) != 0 {
+          fail( "Key could not be deleted." )
+        }
+      }
+      fmt.Printf( "ok.\r\n" )
+    }
+
+
+    
+
+
+    fmt.Printf( "Checking LIST COMMANDS..." )
+    if commands, err := db.ListCommands(); err != nil {
+      fail( err.Error() )
+    } else {
+      if len( commands ) != 27 {
+        fail( "Ivalid result." )
+      }
+    }
+    fmt.Printf( "ok.\r\n" )
+
+
+    fmt.Printf( "Checking LIST CONNECTIONS..." )
+    if connections, err := db.ListConnections(); err != nil {
+      fail( err.Error() )
+    } else {
+      if len( connections ) == 0 {
+        fail( "Ivalid result." )
+      }
+    }
+    fmt.Printf( "ok.\r\n" )
+
+    fmt.Printf( "Checking LIST DATABASE CONNECTIONS (X)..." )
+    if connections, err := db.ListDatabaseConnections( "X" ); err != nil {
+      fail( err.Error() )
+    } else {
+      if len( connections ) == 0 {
+        fail( "Ivalid result." )
+      }
+    }
+    fmt.Printf( "ok.\r\n" )
+
+
+  
+
+    fmt.Printf( "Checking LIST DATABASE CONNECTIONS ID %d...", id )
+    if connections, err := db.ListDatabaseClientConnectionIds( id ); err != nil {
+      fail( err.Error() )
+    } else {
+      if len( connections ) == 0 {
+        fail( "Ivalid result." )
+      }
+    }
+    fmt.Printf( "ok.\r\n" )
+
+    fmt.Printf( "Checking LIST INFO..." )
+    if _, err := db.GetInfo(); err != nil {
+      fail( err.Error() )
+    }
+    fmt.Printf( "ok.\r\n" )
+
+    fmt.Printf( "Checking LIST TABLES..." )
+    if tables, err := db.ListTables(); err != nil {
+      fail( err.Error() )
+    } else {
+      if len( tables ) < 1 {
+        fail( "Ivalid result." )
+      }
+    }
+    fmt.Printf( "ok.\r\n" )
+
+    fmt.Printf( "Checking LIST PLUGINS..." )
+    if _, err := db.ListPlugins(); err != nil {
+      fail( err.Error() )
+    }
+    fmt.Printf( "ok.\r\n" )
+  
+
+    fmt.Printf( "Checking LIST CLIENT KEYS..." )
+    if _, err := db.ListClientKeys(); err != nil {
+      fail( err.Error() )
+    }
+    fmt.Printf( "ok.\r\n" )
 
 
 
 
-		fmt.Printf( "Checking UNUSE DATABASE..." )
-		if err := db.UnuseDatabase(); err != nil { // Database
-			fail( err.Error() )
-		}
-		fmt.Printf( "ok.\r\n" )
+    // fmt.Printf( "Checking LIST NODES..." )
+    // if nodes, err := db.ListNodes(); err != nil {
+    //  fail( err.Error() )
+    // } else {
+    //  if len( nodes ) == 0 {
+    //    fail( "Ivalid result." )
+    //  }
+    // }
+    // fmt.Printf( "ok.\r\n" )
 
-		// fmt.Printf( "Checking CLOSE CONNECTION..." )
-		// if err := db.CloseConnection( "14" ); err != nil { // ConnectionID
-		// 	fail( err.Error() )
-		// }
-		// fmt.Printf( "ok.\r\n" )
-		
+    fmt.Printf( "Checking LIST DATABASE KEYS..." )
+    if _, err := db.ListDatabaseKeys(); err != nil {
+      fail( err.Error() )
+    }
+    fmt.Printf( "ok.\r\n" )
 
-		fmt.Printf( "Success.\r\n" )
-		os.Exit( 0 )
-	}
-	fail( err.Error() )
+
+
+
+    fmt.Printf( "Checking UNUSE DATABASE..." )
+    if err := db.UnuseDatabase(); err != nil { // Database
+      fail( err.Error() )
+    }
+    fmt.Printf( "ok.\r\n" )
+
+    // fmt.Printf( "Checking CLOSE CONNECTION..." )
+    // if err := db.CloseConnection( "14" ); err != nil { // ConnectionID
+    //  fail( err.Error() )
+    // }
+    // fmt.Printf( "ok.\r\n" )
+    
+
+    fmt.Printf( "Success.\r\n" )
+    os.Exit( 0 )
+  }
+  fail( err.Error() )
 }
 
 func fail( Message string ) {
-	fmt.Printf( "failed: %s\r\n", Message )
-	os.Exit( 1 )
+  fmt.Printf( "failed: %s\r\n", Message )
+  os.Exit( 1 )
 }
--- GO/src/Test/test1.go
+++ GO/src/Test/test1.go
@@ -1,26 +1,43 @@
+//
+//                    ////              SQLite Cloud
+//        ////////////  ///             
+//      ///             ///  ///        Product     : SQLite Cloud GO SDK
+//     ///             ///  ///         Version     : 0.0.1
+//     //             ///   ///  ///    Date        : 2021/08/13
+//    ///             ///   ///  ///    Author      : Andreas Pfeil
+//   ///             ///   ///  ///     
+//   ///     //////////   ///  ///      Description : Simple SQLite Cloud server
+//   ////                ///  ///                     test. Creates a table,
+//     ////     //////////   ///                      insertes some values, uses
+//        ////            ////                        C-SDK's Dump function.
+//          ////     /////              
+//             ///                      Copyright   : 2021 by SQLite Cloud Inc.
+//
+// -----------------------------------------------------------------------TAB=2
+
 package main
 
 import "sqlitecloud"
 
 func main() {
-	db, err := sqlitecloud.Connect( "sqlitecloud://***REMOVED***/X" )
-	if err == nil {
-		defer db.Close()
+  db, err := sqlitecloud.Connect( "sqlitecloud://***REMOVED***/X" )
+  if err == nil {
+    defer db.Close()
 
-		db.CreateDatabase( "X", "", "UTF-8", true )
-		db.Execute( `CREATE TABLE IF NOT EXISTS "Dummy" (ID INTEGER PRIMARY KEY AUTOINCREMENT, FirstName TEXT(20), LastName TEXT(20), ZIP INTEGER, City TEXT, Address TEXT)` )
-		db.Execute( `DELETE FROM Dummy` )
-		db.Execute( `INSERT INTO Dummy ( FirstName, LastName, ZIP, City, Address ) VALUES( 'Some', 'One', 96450, 'Coburg', "Mohrenstrasse 1" )` )
-		db.Execute( `INSERT INTO Dummy ( FirstName, LastName, ZIP, City, Address ) VALUES( 'Someone', 'Else', 96145, 'Sesslach', 'Raiffeisenstrasse 6' )` )
-		db.Execute( `INSERT INTO Dummy ( FirstName, LastName, ZIP, City, Address ) VALUES( 'One', 'More', 91099, 'Poxdorf', "Langholzstr. 4" )` )
+    db.CreateDatabase( "X", "", "UTF-8", true )
+    db.Execute( `CREATE TABLE IF NOT EXISTS "Dummy" (ID INTEGER PRIMARY KEY AUTOINCREMENT, FirstName TEXT(20), LastName TEXT(20), ZIP INTEGER, City TEXT, Address TEXT)` )
+    db.Execute( `DELETE FROM Dummy` )
+    db.Execute( `INSERT INTO Dummy ( FirstName, LastName, ZIP, City, Address ) VALUES( 'Some', 'One', 96450, 'Coburg', "Mohrenstrasse 1" )` )
+    db.Execute( `INSERT INTO Dummy ( FirstName, LastName, ZIP, City, Address ) VALUES( 'Someone', 'Else', 96145, 'Sesslach', 'Raiffeisenstrasse 6' )` )
+    db.Execute( `INSERT INTO Dummy ( FirstName, LastName, ZIP, City, Address ) VALUES( 'One', 'More', 91099, 'Poxdorf', "Langholzstr. 4" )` )
 
-		if res, err := db.Select( "SELECT * FROM Dummy" ); res != nil {
-			defer res.Free()
+    if res, err := db.Select( "SELECT * FROM Dummy" ); res != nil {
+      defer res.Free()
 
-			if err == nil {
-				res.Dump()
-				return
+      if err == nil {
+        res.Dump()
+        return
 
-	} } }
-	panic( err )
+  } } }
+  panic( err )
 }
