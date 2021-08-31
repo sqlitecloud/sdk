089c79df253be227766ad0ca000a016dee0b5b44
Andreas Pfeil <patreon@familie-pfeil.com>
2021-08-31 23:09:07 +0200

    Cleanup

--- GO/src/Test/test1.go
+++ GO/src/Test/test1.go
@@ -25,18 +25,6 @@ func main() {
   if err == nil {
     defer db.Close()
 
-    //db.Compress( "lz4" )
-
-    if res, err := db.Select( "GET LOAD" ); res != nil {
-      defer res.Free()
-
-      f, e := res.GetFloat32()
-      fmt.Printf( "Load = %f (%v)\r\n", f, e )
-    } else {
-       panic( err )
-     }
-
-
     db.CreateDatabase( "X", "", "UTF-8", true )
     db.Execute( `CREATE TABLE IF NOT EXISTS "Dummy" (ID INTEGER PRIMARY KEY AUTOINCREMENT, FirstName TEXT(20), LastName TEXT(20), ZIP INTEGER, City TEXT, Address TEXT)` )
     db.Execute( `DELETE FROM Dummy` )
