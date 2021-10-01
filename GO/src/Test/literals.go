5d5253f7627337db1f82b364224ac59e2bd7a0b4
Andreas Pfeil <patreon@familie-pfeil.com>
2021-10-01 10:37:01 +0200

    API improvements

--- GO/src/Test/literals.go
+++ GO/src/Test/literals.go
@@ -1,66 +1,63 @@
 //
 //                    ////              SQLite Cloud
-//        ////////////  ///             
+//        ////////////  ///
 //      ///             ///  ///        Product     : SQLite Cloud GO SDK
-//     ///             ///  ///         Version     : 1.0.0
-//     //             ///   ///  ///    Date        : 2021/08/31
+//     ///             ///  ///         Version     : 1.1.0
+//     //             ///   ///  ///    Date        : 2021/10/01
 //    ///             ///   ///  ///    Author      : Andreas Pfeil
-//   ///             ///   ///  ///     
+//   ///             ///   ///  ///
 //   ///     //////////   ///  ///      Description : Simple SQLite Cloud server
 //   ////                ///  ///                     test. Creates a table,
 //     ////     //////////   ///                      insertes some values, uses
 //        ////            ////                        C-SDK's Dump function.
-//          ////     /////              
+//          ////     /////
 //             ///                      Copyright   : 2021 by SQLite Cloud Inc.
 //
 // -----------------------------------------------------------------------TAB=2
 
 package main
 
-import "fmt"
-import "sqlitecloud"
+import (
+  "errors"
+  "fmt"
+  "sqlitecloud"
+)
 
 func main() {
-  db, err := sqlitecloud.Connect( "sqlitecloud://dev1.sqlitecloud.io/X" )
-  if err == nil {
+  var db    *sqlitecloud.SQCloud
+  var res   *sqlitecloud.Result
+  var err   error
+
+  if db, err = sqlitecloud.Connect( "sqlitecloud://dev1.sqlitecloud.io/X" ); err == nil {   
     defer db.Close()
 
-    // String Literal
-    if res, err := db.Select( "PING" ); res != nil {
-      defer res.Free()
-
-      if err != nil {                             panic( err ) }
-      if s, err := res.GetString(); err != nil {  panic( err )
-      } else {                                    fmt.Printf( "String = %s\r\n", s ) }
-    } else {                                      panic( err ) }
-
-     // Integer
-     if res, err := db.Select( "GET DATABASE ID" ); res != nil {
-      defer res.Free()
-      
-      if err != nil {                             panic( err ) }
-      if i, err := res.GetInt32(); err != nil {   panic( err )
-     } else {                                     fmt.Printf( "Integer = %d\r\n", i ) }
-    } else {                                      panic( err ) }
-
-    // Float
-    if res, err := db.Select( "GET LOAD" ); res != nil {
-      defer res.Free()
-      
-      if err != nil {                             panic( err ) }
-      if f, err := res.GetFloat32(); err != nil { panic( err )
-      } else {                                    fmt.Printf( "Float = %f\r\n", f ) }
-    } else {                                      panic( err ) }
-
-    // Error
-    if res, err := db.Select( "UNKNOWN COMMAND" ); res != nil {
-     defer res.Free()
-
-    } else {    
-      if err != nil {                             fmt.Printf( "Error = %v\r\n", err ) }
+    switch res, err = db.Select( "PING" ); {            // String Literal
+    case err != nil:        panic( err )
+    case res == nil:        panic( errors.New( "" ) )
+    case !res.IsString():   panic( errors.New( "" ) )
+    default:                fmt.Printf( "String = %s\r\n", res.GetString_() )
     }
-   
-     return
-  }
-  panic( err )
+    res.Free()
+
+    switch res, err = db.Select( "GET DATABASE ID" ); { // Integer
+    case err != nil:        panic( err )
+    case res == nil:        panic( errors.New( "" ) )
+    case !res.IsInteger():  panic( errors.New( "" ) )
+    default:                fmt.Printf( "Integer = %d\r\n", res.GetInt32_() )
+    }
+    res.Free()
+      
+    switch res, err = db.Select( "GET LOAD" ); {        // Float
+    case err != nil:        panic( err )
+    case res == nil:        panic( errors.New( "" ) )
+    case !res.IsFloat():    panic( errors.New( "" ) )
+    default:                fmt.Printf( "Float = %f\r\n", res.GetFloat32_() )
+    }
+    res.Free()
+
+    switch res, err = db.Select( "UNKNOWN COMMAND" ); { // Error
+    case err == nil:        panic( errors.New( "Unknown command returned no error" ) )
+    default:                fmt.Printf( "Error = %v\r\n", err )
+    }
+  } else { panic( err ) }
 }
