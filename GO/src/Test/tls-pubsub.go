8fdfebefcc8a2e7c18f799c0538d50c4a34bd377
Andreas Pfeil <patreon@familie-pfeil.com>
2021-10-12 22:00:42 +0200

    ADD: PSUB Support

---
+++ GO/src/Test/tls-pubsub.go
@@ -0,0 +1,41 @@
+//
+//                    ////              SQLite Cloud
+//        ////////////  ///             
+//      ///             ///  ///        Product     : SQLite Cloud GO SDK
+//     ///             ///  ///         Version     : 1.0.0
+//     //             ///   ///  ///    Date        : 2021/10/11
+//    ///             ///   ///  ///    Author      : Andreas Pfeil
+//   ///             ///   ///  ///     
+//   ///     //////////   ///  ///      Description : Simple PSUB Test
+//   ////                ///  ///       
+//     ////     //////////   ///        
+//        ////            ////          
+//          ////     /////              
+//             ///                      Copyright   : 2021 by SQLite Cloud Inc.
+//
+// -----------------------------------------------------------------------TAB=2
+
+package main
+
+import "fmt"
+import "time"
+import "sqlitecloud"
+
+func main() {
+  fmt.Printf( "Simple PSUB test...\r\n")
+  
+  if db, _ := sqlitecloud.Connect( "sqlitecloud://***REMOVED***:9860/X?tls=NO" ); db != nil {
+    defer db.Close()
+
+    db.Callback = func( JSON string ) {
+      println( "The json string was: " + JSON )
+    }
+      
+    db.Listen( "Channel" )
+
+    db.Notify( "Channel" )
+    db.SendNotificationMessage( "Channel", "Hello dear fellows!" )
+    
+    println( "Waiting for Messages, press CTL+C to exit..." )
+    time.Sleep( 10000 * time.Second )
+} }
