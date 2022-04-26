d85f1336dad41f0e00059827658bdcc953a1f458
Andrea Donetti <andinux@gmail.com>
2022-04-25 22:01:27 -0500

    add info from LIST NODES command to the {projectID}/nodes/GET.lua endpoint
    the id returned by LIST NODE must match the new node_id column from the Node table (auth db)

--- GO/src/sqliteweb/dashboard/v1/{projectID}/nodes/GET.lua
+++ GO/src/sqliteweb/dashboard/v1/{projectID}/nodes/GET.lua
@@ -26,17 +26,22 @@ local userID,    err, msg = checkUserID( userid )                        if err 
 local projectID, err, msg = checkProjectID( projectID )                  if err ~= 0 then return error( err, msg )                     end
 
 Node = {
-  id        = 0,                                -- NodeID - It is not good to have a simple int number!!!!!!
-  name      = "",                               -- Name of this node
-  type      = "",                               -- Type fo this node, for example: Leader, Worker
-  provider  = "",                               -- Provider of this node
-  image     = "",                               -- Image data for this node
-  region    = "",                               -- Regin data for this node
-  size      = "",                               -- Size info for this node
-  address   = "",                               -- IPv[4,6] address or host name of this node
-  port      = "",                                -- Port this node is listening on
-  latitude  = 44.931,
-  longitude = 10.533,
+  id            = 0,                                -- NodeID - It is not good to have a simple int number!!!!!!
+  name          = "",                               -- Name of this node
+  type          = "",                               -- Type fo this node, for example: Leader, Worker
+  provider      = "",                               -- Provider of this node
+  image         = "",                               -- Image data for this node
+  region        = "",                               -- Regin data for this node
+  size          = "",                               -- Size info for this node
+  address       = "",                               -- IPv[4,6] address or host name of this node
+  port          = "",                               -- Port this node is listening on
+  latitude      = 44.931,
+  longitude     = 10.533,
+  node_id       = 0,                                -- id of the node inside de cluster
+  status        = "",                               -- raft status of the node in the cluster (LIST NODES)
+  progress      = "",                               -- progress is in one of the three statethree state: probe, replicate, snapshot. (LIST NODES)
+  match         = 0,                                -- is the index of the highest known matched raft entry (LIST NODES)
+  last_activity = "",                               -- date and time of the last contact with a follower. Leader has NULL. (LIST NODES)
 }
 
 Response = {
@@ -74,16 +79,36 @@ if userID == 0 then
 else
   
   local projectID, err, msg = verifyProjectID( userID, projectID )         if err ~= 0 then return error( err, msg )                     end
-  local query     = string.format( "SELECT NODE.id, NODE.name, type, provider, image AS details, region, size, IIF( addr4, addr4, '' ) || IIF( addr4 AND addr6, ',', '' ) || IIF( addr6, addr6, '' ) AS address, port, latitude, longitude FROM USER JOIN PROJECT ON USER.id = PROJECT.user_id JOIN NODE ON PROJECT.uuid = NODE.project_uuid WHERE USER.enabled = 1 AND USER.id = %d AND uuid='%s';", userID, enquoteSQL( projectID ) )
+  local query     = string.format( "SELECT NODE.id, NODE.name, type, provider, image AS details, region, size, IIF( addr4, addr4, '' ) || IIF( addr4 AND addr6, ',', '' ) || IIF( addr6, addr6, '' ) AS address, port, latitude, longitude, node_id FROM USER JOIN PROJECT ON USER.id = PROJECT.user_id JOIN NODE ON PROJECT.uuid = NODE.project_uuid WHERE USER.enabled = 1 AND USER.id = %d AND uuid='%s';", userID, enquoteSQL( projectID ) )
   --print( query )
   local databases = nil
 
-  
   nodes = executeSQL( "auth", query )
   if not nodes                            then return error( 404, "ProjectID not found" ) end
   if nodes.ErrorNumber              ~= 0  then return error( 502, "Bad Gateway" )         end
-  if nodes.NumberOfColumns          ~= 11 then return error( 502, "Bad Gateway" )         end
+  if nodes.NumberOfColumns          ~= 12 then return error( 502, "Bad Gateway" )         end
   if nodes.NumberOfRows             <  1  then return error( 200, "OK" )                  end
+
+  query = "LIST NODES"
+  listNodes = executeSQL( projectID, query )
+  if (listNodes.NumberOfColumns == 8) then  
+    for i = 1, nodes.NumberOfRows do
+      cluster_node_id = nodes.Rows[ i ].node_id
+
+      -- lookup which row from LIST NODES matches the cluster_node_id of the current row
+      row = 0
+      for j = 1, listNodes.NumberOfRows do
+        if listNodes.Rows[ j ].id == cluster_node_id then row = j break end
+      end
+
+      if row > 0 then
+        nodes.Rows[ i ].status = listNodes.Rows[ row ].status 
+        nodes.Rows[ i ].progress = listNodes.Rows[ row ].progress 
+        nodes.Rows[ i ].match = listNodes.Rows[ row ].match
+        nodes.Rows[ i ].last_activity = listNodes.Rows[ row ].last_activity 
+      end
+    end  
+  end
 
   Response.nodes = nodes.Rows
   
