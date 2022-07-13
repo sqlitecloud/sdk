43a7adac13b6acea76660869cc6b6083f3a82acd
Andrea Donetti <andinux@gmail.com>
2022-07-13 09:13:16 +0200

    fix(sqliteweb): update the project/user/company query in the user DELETE endpoint. TODO: create endpoints to manage the Customer table, during the deletion of a customer we must delete its projects and nodes.

--- GO/src/sqliteweb/admin/v1/user/{email}/DELETE.lua
+++ GO/src/sqliteweb/admin/v1/user/{email}/DELETE.lua
@@ -17,12 +17,12 @@
 
 -- https://localhost:8443/admin/v1/user/{email}
 
--- SELECT NODE.id FROM NODE JOIN PROJECT ON PROJECT.uuid = NODE.project_uuid JOIN USER ON USER.id = PROJECT.user_id WHERE USER.email='my.address@domain.com'
--- DELETE FROM NodeSettings WHERE node_id = id
+-- -- SELECT NODE.id AS nodeID FROM Node JOIN Project ON Project.uuid = Node.project_uuid JOIN Company ON Company.id = Project.company_id JOIN User ON User.company_id = Company.id WHERE USER.email='my.address@domain.com'
+-- -- DELETE FROM NodeSettings WHERE node_id = id
 
--- SELECT id FROM USER WHERE email =
--- DELETE FROM UserSettings WHERE user_id = id
--- DELETE FROM PROJECT WHERE user_id = id
+-- -- SELECT id FROM USER WHERE email =
+-- -- DELETE FROM UserSettings WHERE user_id = id
+-- -- DELETE FROM PROJECT WHERE user_id = id
 -- DELETE FROM USER WHERE id = id 
 
 require "sqlitecloud"
@@ -42,25 +42,29 @@ if userID.NumberOfRows    ~= 1                                                  
 
 local userID,    err, msg = checkUserID( userID.Rows[ 1 ].id  )          if err ~= 0 then return error( err, msg )                            end
 
-query = string.format( "SELECT NODE.id FROM NODE JOIN PROJECT ON PROJECT.uuid = NODE.project_uuid WHERE PROJECT.user_id='%d';", userID )
-nodes = executeSQL( "auth", query )
+-- TODO: missing APIs to manage the Customer table and to remove projects and node for deleted companies, 
+--       we cannot delete projects and nodes here with the following code because other users can still 
+--       exist for the same Company
 
-if not nodes                                                                         then return error( 504, "Gateway Timeout" )              end
-if nodes.ErrorMessage    ~= ""                                                       then return error( 502, userID.ErrorMessage )            end
-if nodes.ErrorNumber     ~= 0                                                        then return error( 502, "Bad Gateway" )                  end
+-- query = string.format( "SELECT NODE.id AS nodeID FROM Node JOIN Project ON Project.uuid = Node.project_uuid JOIN Company ON Company.id = Project.company_id JOIN User ON User.company_id = Company.id WHERE User.id ='%d';", userID )
+-- nodes = executeSQL( "auth", query )
+
+-- if not nodes                                                                         then return error( 504, "Gateway Timeout" )              end
+-- if nodes.ErrorMessage    ~= ""                                                       then return error( 502, userID.ErrorMessage )            end
+-- if nodes.ErrorNumber     ~= 0                                                        then return error( 502, "Bad Gateway" )                  end
 
 transaction = "BEGIN TRANSACTION;"
 
-if nodes.NumberOfRows > 0 then
-  if nodes.NumberOfColumns ~= 1                                                      then return error( 502, "Bad Gateway" )                  end
+-- if nodes.NumberOfRows > 0 then
+--   if nodes.NumberOfColumns ~= 1                                                      then return error( 502, "Bad Gateway" )                  end
 
-  for i = 1, nodes.NumberOfRows do
-    transaction = string.format( "%s DELETE FROM NodeSettings WHERE node_id=%d;", transaction, nodes.Rows[ i ].id )
-  end
-end
+--   for i = 1, nodes.NumberOfRows do
+--     transaction = string.format( "%s DELETE FROM NodeSettings WHERE node_id=%d;", transaction, nodes.Rows[ i ].id )
+--   end
+-- end
 
 transaction = string.format( "%s DELETE FROM UserSettings WHERE user_id=%d;", transaction, userID )
-transaction = string.format( "%s DELETE FROM Project WHERE user_id=%d;"     , transaction, userID )
+-- transaction = string.format( "%s DELETE FROM Project WHERE user_id=%d;"     , transaction, userID )
 transaction = string.format( "%s DELETE FROM User WHERE id=%d;"             , transaction, userID )
 transaction = string.format( "%s COMMIT TRANSACTION;"                       , transaction         )
 
