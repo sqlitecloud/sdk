487663d8524a195874263e50d8f9ddff59962b10
Gionata Mettifogo <gmettifogo@gmail.com>
2023-11-20 09:31:58 +0100

    Removing previous version of the node library

--- NODEJS/src/core/sqlite-cloud-licence.js
+++
@@ -1,7 +0,0 @@
-/*!
- * SQLite Cloud Node.js SDK v<VERSION>
- * https://sqlitecloud.io/
- *
- * Copyright 2023, SQLite Cloud
- * Released under the MIT licence.
- */
--- NODEJS/webpack/hosting_config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  version: require('../package').version,
-};
